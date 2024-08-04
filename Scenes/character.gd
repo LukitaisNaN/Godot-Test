3502a22fee61c8654aeb593c78b8177d0eab756b
lukita <luca.irrazabal@mi.unc.edu.ar>
2024-08-03 21:05:12 -0300

    added lamp, lights, bigger floor and camera movement

--- Scenes/character.gd
+++ Scenes/character.gd
@@ -1,11 +1,19 @@
 extends CharacterBody3D
 
+# Movement constants 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const MOUSE_SENSIVITY = 0.1
+
+# Rotation variables
+var prev_mouse_pos
+var next_mouse_pos
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,7 +25,6 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -28,3 +35,10 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+	pass
+
+func _input(event):
+	if event is InputEventMouse:
+		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENSIVITY))
+	
