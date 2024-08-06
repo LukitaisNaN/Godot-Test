1fe14dd58b30e53adf2a6ab395d7e5e5b6a18e64
lukita <luca.irrazabal@mi.unc.edu.ar>
2024-08-06 11:53:30 -0300

    online working

--- Scenes/character.gd
+++ Scenes/character.gd
@@ -1,18 +1,27 @@
 extends CharacterBody3D
 
+@onready var camera = $Camera3D
+
 # Movement constants 
 const SPEED = 10.0
 const JUMP_VELOCITY = 9
-const MOUSE_SENSIVITY = 0.1
+const MOUSE_SENSIVITY = 0.01
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 20
 
+func _enter_tree():
+	set_multiplayer_authority(str(name).to_int())
+	pass
+
 func _ready():
-	# Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	if not is_multiplayer_authority(): return
+	# Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	camera.current = true
 	pass
 
 func _physics_process(delta):
+	if not is_multiplayer_authority(): return
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -36,6 +45,11 @@ func _physics_process(delta):
 	pass
 
 func _input(event):
+	if not is_multiplayer_authority(): return
+	
 	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENSIVITY))
+		rotate_y(-event.relative.x * MOUSE_SENSIVITY)
+		camera.rotate_x(-event.relative.y * MOUSE_SENSIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+		
 	
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -6,15 +6,7 @@ const PORT = 9999
 var enet_peer = ENetMultiplayerPeer.new()
 
 const PLAYER = preload("res://Scenes/character.tscn")
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+	
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
@@ -35,14 +27,21 @@ func _on_host_button_pressed():
 func _on_join_button_pressed():
 	main_menu.hide()
 	
-	var _ip_address = $Control/MarginContainer/VBoxContainer/ipButton
-	
 	enet_peer.create_client("localhost", PORT)
 	multiplayer.multiplayer_peer = enet_peer
-	
-	pass # Replace with function body.
+	multiplayer.connection_failed.connect(failed)
+	pass
 
 func add_player(peer_id):
 	var player = PLAYER.instantiate()
 	player.name = str(peer_id)
+	print("Connection success!")
 	add_child(player)
+	
+	pass
+	
+func failed():
+	main_menu.show()
+	
+	pass
+	
